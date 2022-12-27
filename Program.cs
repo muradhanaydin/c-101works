3737f2b80dc7f3ccb8e76c465f27effc2ae645e1
MURADHAN AYDIN <muradhanaydin@gmail.com>
2022-12-27 19:19:35 +0300

    new works added [strings]

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ namespace c__101_work{
             for(int s=0; s<n; s++){
                 Console.Write($"{arr[n-s-1]}\t");
             }
-            */
+            
             //ODEV 4
             Console.Write("Bir cumle giriniz: ");
             string text = Console.ReadLine();
@@ -54,6 +54,55 @@ namespace c__101_work{
 
             Console.WriteLine($"Metindeki kelime sayisi: {words}\nMetindeki harf sayisi: {chars}");
             
+            */
+
+            string t1 = "Bugun hava cok guzel degilmi?";
+            string t2 = "guzel degilmi";
+
+            //Length
+            Console.WriteLine(t1.Length);
+
+            //Toupper Tolower
+            Console.WriteLine(t1.ToLower());
+            Console.WriteLine(t2.ToUpper());
+
+            //Concat
+            Console.WriteLine(string.Concat(t1 , "Ahmet"));
+
+            //Compareto compare
+            Console.WriteLine(t1.CompareTo(t2));
+            Console.WriteLine(string.Compare(t1,t2,true));
+            Console.WriteLine(string.Compare(t1,t2,false));
+
+            //Contains
+            Console.WriteLine(t1.Contains(t2));
+            Console.WriteLine(t1.EndsWith("degilmi"));
+            Console.WriteLine(t1.StartsWith("Bugun"));
+
+            //IndexOf
+            Console.WriteLine(t1.IndexOf("cok"));
+            Console.WriteLine(t2.IndexOf("e"));
+            Console.WriteLine(t1.LastIndexOf("?"));
+
+            //Insert
+            Console.WriteLine(t1.Insert(0 , t1+" "));
+
+            //PadLeft PadRight
+            Console.WriteLine(t1 + t2.PadLeft(30));
+            Console.WriteLine(t1 + t2.PadLeft(30,'*'));
+            Console.WriteLine(t1.PadRight(30) + t2);
+            Console.WriteLine(t1.PadRight(30,'_') + t2);
+
+            //Remove
+            Console.WriteLine(t1.Remove(t1.Length -5));
+            Console.WriteLine(t1.Remove(5,10));
+
+            //Replace
+            Console.WriteLine(t1.Replace("Bugun","BUGUN"));
+            Console.WriteLine(t1.Replace(' ' , '*'));
+
+            //Split
+            Console.WriteLine(t1.Split(" ")[0]);
 
         }
     }
